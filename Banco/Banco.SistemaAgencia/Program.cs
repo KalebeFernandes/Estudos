d306f9d5a228485304be71df6cef731a5da43556
Kalebe Fernandes Mendes Pereira <kallfrenandes@hotmail.com>
2019-06-03 22:35:07 -0300

    Projeto Agencia

---
+++ Banco/Banco.SistemaAgencia/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pagina?argumentos
+            // 012345678
+
+
+
+            string textoVazio = "";
+            string textoNulo = null;
+            string textoQualquer = "defergfer";
+
+
+
+            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
+
+            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+
+            int indiceInterrogacao = url.IndexOf('?');
+
+            Console.WriteLine(indiceInterrogacao);
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(indiceInterrogacao + 1);
+            Console.WriteLine(argumentos);
+
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
