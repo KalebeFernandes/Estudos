d0a946adfdfbfc1070767e08265a7d36f1bbd7d2
Kalebe Fernandes Mendes Pereira <kallfrenandes@hotmail.com>
2019-06-11 21:12:28 -0300

    Substring

--- Banco/Banco.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
+++ Banco/Banco.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -5,20 +5,29 @@ namespace ByteBank.SistemaAgencia
     public class ExtratorValorDeArgumentosURL
     {
         public string URL { get; }
+        private readonly string _argumntos;
 
         public ExtratorValorDeArgumentosURL(string url)
         {
-            if (url == null)
-            {
-                throw new ArgumentNullException(nameof(url));
-            }
-
-            if (url == "")
+            if (string.IsNullOrEmpty(url))
             {
                 throw new ArgumentException("O argumento url não pode ser uma string vazia.", nameof(url));
             }
-
             URL = url;
+            var indiceInterrogacao = url.IndexOf('?');
+            _argumntos = url.Substring(indiceInterrogacao + 1);
+        }
+
+        public string GetValor(string parametro)
+        {
+            string palavra = "moedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino";
+
+            int indice = palavra.IndexOf(nomeArgumento);
+
+            int indiceValor = indice + nomeArgumento.Length;
+
+            string valorArgumento = palavra.Substring(indiceValor);
         }
     }
 }
--- Banco/Banco.SistemaAgencia/Program.cs
+++ Banco/Banco.SistemaAgencia/Program.cs
@@ -6,29 +6,32 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
             // pagina?argumentos
             // 012345678
-
-
-
             string textoVazio = "";
             string textoNulo = null;
             string textoQualquer = "defergfer";
 
-
-
-            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
-
-            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+            var extrator = new ExtratorValorDeArgumentosURL(url);
+            
+            Console.WriteLine(url);
 
             int indiceInterrogacao = url.IndexOf('?');
-
             Console.WriteLine(indiceInterrogacao);
-
-            Console.WriteLine(url);
+           
             string argumentos = url.Substring(indiceInterrogacao + 1);
             Console.WriteLine(argumentos);
+            Console.WriteLine(string.IsNullOrEmpty(textoVazio));
+            Console.WriteLine(string.IsNullOrEmpty(textoNulo));
+            Console.WriteLine(string.IsNullOrEmpty(textoQualquer));
 
+            var palavra = url;
+            var nomeArgumento = "moedaDestino";
+            var indice = palavra.IndexOf(nomeArgumento);
+            Console.WriteLine(indice);
+            Console.WriteLine(palavra.Substring(indice));
+            Console.WriteLine(indice + nomeArgumento.Length);
 
             Console.ReadLine();
         }
