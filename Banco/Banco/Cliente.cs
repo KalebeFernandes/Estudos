0047ca9f6fd1d278d1c413370b52236a4278c8b0
Kalebe Fernandes Mendes Pereira <kallfrenandes@hotmail.com>
2019-06-20 10:37:21 -0300

    Curso de String, alura

--- Banco/Banco/Cliente.cs
+++ Banco/Banco/Cliente.cs
@@ -5,5 +5,11 @@
         public string Nome { get; set; }
         public string CPF { get; set; }
         public string Profissao { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            Cliente este = obj as Cliente;
+            return este != null && este.CPF == CPF;
+        }
     }
 }
--- Banco/Banco/ContaCorrente.cs
+++ Banco/Banco/ContaCorrente.cs
@@ -107,6 +107,10 @@ namespace ByteBank.Modelos
 
             contaDestino.Depositar(valor);
         }
-    }
 
+        public override string ToString()
+        {
+            return $"Número {Numero}, Agência {Agencia}, Saldo {Saldo}";
+        }
+    }
 }
